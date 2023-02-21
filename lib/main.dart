f7ac6ffcfd4511ef4e1474a694652151871b1f36
Valentin <Valentin@DESKTOP-72LQMEF.(none)>
2023-02-21 17:26:29 +0100

    icons dl

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-
 //import 'package:google_fonts/google_fonts.dart';
 
 import 'sandbox/sandbox.dart';
+
+import 'navigation/navigation.dart';
 import 'navigation/home.dart';
 
 void main() {
