e56cf4259f97b17ba752644c49ec94f51f3c0e15
Valentin <luginbuhlvalentin@gmail.com>
2024-04-16 21:10:33 +0200

    update

--- .dart_tool/flutter_build/7adbe5b92c399e04602e1c0ef6c23c20/native_assets.d
+++
@@ -1,1 +0,0 @@
- /Users/lugin/Projects/GitHub/brainine-app/.dart_tool/flutter_build/7adbe5b92c399e04602e1c0ef6c23c20/native_assets.yaml: 
--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/lugin/Projects/GitHub/brainine-app/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/lugin/Projects/GitHub/brainine-app/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m
